09b0ca94ecf1645af6e71fe1fafa9a54167e2266
hezron <hezzykialo.official.ke@gmail.com>
2022-07-15 16:12:56 +0300

    string function

---
+++ app.py
@@ -0,0 +1,15 @@
+# variable
+name = "Hezron"
+
+print ("Hello"+ " "+ name)
+
+# datatypes
+# string functions, index,uppercase,lowercase,len
+# isupper used to return a boolean (TRUE or FALSE  )
+print(name[1].upper())
+
+print(len(name))
+# index of character
+print(name.index('z'))
+# replace
+print(name.replace('z', 'M'))
