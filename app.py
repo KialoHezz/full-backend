d127637b84f5c044b890021169cfe216ec98d59d
hezron <hezzykialo.official.ke@gmail.com>
2022-07-15 16:52:21 +0300

    list

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-# variable
-name = "Hezron"
+# list > index number
+# list(())
+countries =  ['kenya', 'Uganda', 'somalia','U.S.A']
+print(countries[0])
+print(countries[0][1].upper())
+print(countries[1:])
 
-print ("Hello"+ " "+ name)
 
-# datatypes > string,numbers
-# string functions, index,uppercase,lowercase,len
-# isupper used to return a boolean (TRUE or FALSE  )
-print(name[1].upper())
-print(len(name))
-# index of character
-print(name.index('z'))
-# replace
-print(name.replace('z', 'M'))
-# value type
-print(type(name))
-# numbers > operation multiplication *,division /,addition +,subtraction - ,modulor %
 
-sentence = input('enter your sentence')
 
-word1 = input('enter the word to replace: ')
-word2 = input('enter the word to replace it with: ')
+# # variable
+# name = "Hezron"
 
-print(sentence.replace(word1, word2))
+# print ("Hello"+ " "+ name)
+
+# # datatypes > string,numbers
+# # string functions, index,uppercase,lowercase,len
+# # isupper used to return a boolean (TRUE or FALSE  )
+# print(name[1].upper())
+# print(len(name))
+# # index of character
+# print(name.index('z'))
+# # replace
+# print(name.replace('z', 'M'))
+# # value type
+# print(type(name))
+# # numbers > operation multiplication *,division /,addition +,subtraction - ,modulor %
+
+# sentence = input('enter your sentence')
+
+# # word1 = input('enter the word to replace: ')
+# # word2 = input('enter the word to replace it with: ')
+
+# # print(sentence.replace(word1, word2))
+
