6dc3e9b31622a8e288efdcdc9a4ed01e6151c18f
hezron <hezzykialo.official.ke@gmail.com>
2022-07-16 20:40:15 +0300

    form ,csrf_token,AVOIDANCE INFORMATION TO URL{POST $ GET}

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index , name='homepage'),
+    path('', views.index , name='homepage'),
+    path('counter/', views.counter , name='counter'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
+# Inside dictionary have Key and Value ,therefore ,to access value you can use the key
 def index(request):
-    return render(request, 'blog/home.html')
+    name = 'Hezron'
+    context = {
+        'name':name
+    }
+    return render(request, 'blog/home.html',context)
+
+
+def counter(request):
+    words = request.POST['words']
+    amount_of_words = len(words.split())
+    ctx = {
+        "amount": amount_of_words,
+    }
+    return render(request, 'blog/counter.html',ctx)
