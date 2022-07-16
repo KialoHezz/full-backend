875d885a133a20141c49f467f3f2765627f2d8d6
hezron <hezzykialo.official.ke@gmail.com>
2022-07-16 14:47:09 +0300

    Simple signUp(Register) and SignIn(Login)

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
-from new import Student
-# OBJECTS $ CLASS
+print("Create Account Now")
+
+username = input("Enter username : ")
+password = input("Enter password : ")   
+confirm_password = input("Enter confirm password : ")
+print("Account created successfully")
+print("Login Now")
+
+username1 = input("Enter username: ")
+password1 = input("Enter password: ")
+
+
+if username == username1 and password == password1:
+    print("logined Successfully")
+else:
+    print("logined Failure or invalid credential")
+
+
+# Loop hole > To validate the password with confirm_password when user is registering
+
+
+
+
+
+
+
+# from new import Student
+# # OBJECTS $ CLASS
 # class Person():
 #     def __init__(self, name, age):
 #         self.name = name
@@ -10,12 +36,12 @@ from new import Student
 
 # print(name,age)
 
-# Inheritance
-class Person(Student):
-    pass
+# # Inheritance
+# class Person(Student):
+#     pass
 
-p1 = Person()
-print(p1.name, p1.age)
+# p1 = Person()
+# print(p1.name, p1.age)
 
 
 
